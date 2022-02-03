b22b1f2379a196ff797b8526963864f8cbb42eb0
Purencool <purencool@gmail.com>
2022-02-04 08:00:37 +1100

    Refactoring so the settings view is separate from the functionality

--- queue-refresh/queue-refresh.js
+++ queue-refresh/queue-refresh.js
@@ -8,120 +8,148 @@ var secondCounter = 0;
 
 /**
  * 
- * @returns void
+ * @returns mixed
  */
-setInterval(function() { mainbody();}, 1000);
+function mainbody() {
+    secondCounter = secondCounter + 1;
+    let utilityBar = document.querySelector(".oneUtilityBar.slds-utility-bar_container > ul");
+    if (utilityBar === null) {
+        return;
+    }
+
+
+    if (utilityBar.lastElementChild.innerText !== "SCE settings") {
+        let li = document.createElement("li");
+        li.id = "service-console-extension-btn";
+        li.innerText = "SCE settings";
+        utilityBar.append(li);
+        configuration();
+        addMenu();
+    }
+
+    queueTimer(secondCounter);
+}
 
 /**
  * 
  * @returns void
  */
 function addMenu() {
-  console.log("Local storage: " + localStorage.getItem("refreshTimer"));
-  let divEle = document.createElement("div");
-  let pEle = document.createElement("p");
-  divEle.id = "service-console-extension-menu";
-  divEle.classList.toggle("service-console-extension-disabled");
-  pEle.innerText = "Select how often the queue refreshes:";
-  divEle.append(pEle);
+    console.log("Local storage: " + localStorage.getItem("refreshTimer"));
+    let divEle = document.createElement("div");
+    let pEle = document.createElement("p");
+    divEle.id = "service-console-extension-menu";
+    divEle.classList.toggle("service-console-extension-disabled");
+    pEle.innerText = "Select how often the queue refreshes:";
+    divEle.append(pEle);
 
-  for (i = 0; i < 5; i++) {
-    let inputEle = document.createElement("input");
-    let labelEle = document.createElement("label");
-    let brEle = document.createElement("br");
-    switch(i) {
-      case 0:
-        inputEle.type = "radio";
-        inputEle.id = "never";
-        inputEle.name = "timer";
-        inputEle.value = "0";
-        if (localStorage.getItem("refreshTimer") === 0) {
-          inputEle.checked = "checked";
-        } else if (localStorage.getItem("refreshTimer") === null) {
-          inputEle.checked = "checked";
+    for (i = 0; i < 5; i++) {
+        let inputEle = document.createElement("input");
+        let labelEle = document.createElement("label");
+        let brEle = document.createElement("br");
+        switch (i) {
+            case 0:
+                inputEle.type = "radio";
+                inputEle.id = "never";
+                inputEle.name = "timer";
+                inputEle.value = "0";
+                if (localStorage.getItem("refreshTimer") === 0) {
+                    inputEle.checked = "checked";
+                } else if (localStorage.getItem("refreshTimer") === null) {
+                    inputEle.checked = "checked";
+                }
+                inputEle.onclick = function () {
+                    storeTimer(0);
+                };
+                labelEle.for = "never";
+                labelEle.innerText = "Never";
+                divEle.append(inputEle);
+                divEle.append(labelEle);
+                divEle.append(brEle);
+                break;
+            case 1:
+                inputEle.type = "radio";
+                inputEle.id = "30";
+                inputEle.name = "timer";
+                inputEle.value = "30";
+                if (localStorage.getItem("refreshTimer") === 30) {
+                    inputEle.checked = "checked";
+                }
+                inputEle.onclick = function () {
+                    storeTimer(30);
+                };
+                labelEle.for = "30";
+                labelEle.innerText = "Every 30 seconds";
+                divEle.append(inputEle);
+                divEle.append(labelEle);
+                divEle.append(brEle);
+                break;
+            case 2:
+                inputEle.type = "radio";
+                inputEle.id = "120";
+                inputEle.name = "timer";
+                inputEle.value = "120";
+                if (localStorage.getItem("refreshTimer") === 120) {
+                    inputEle.checked = "checked";
+                }
+                inputEle.onclick = function () {
+                    storeTimer(120);
+                };
+                labelEle.for = "120";
+                labelEle.innerText = "Every two minutes";
+                divEle.append(inputEle);
+                divEle.append(labelEle);
+                divEle.append(brEle);
+                break;
+            case 3:
+                inputEle.type = "radio";
+                inputEle.id = "300";
+                inputEle.name = "timer";
+                inputEle.value = "300";
+                if (localStorage.getItem("refreshTimer") === 300) {
+                    inputEle.checked = "checked";
+                }
+                inputEle.onclick = function () {
+                    storeTimer(300);
+                };
+                labelEle.for = "300";
+                labelEle.innerText = "Every five minutes";
+                divEle.append(inputEle);
+                divEle.append(labelEle);
+                divEle.append(brEle);
+                break;
+            case 4:
+                inputEle.type = "radio";
+                inputEle.id = "1500";
+                inputEle.name = "timer";
+                inputEle.value = "1500";
+                if (localStorage.getItem("refreshTimer") === 1500) {
+                    inputEle.checked = "checked";
+                }
+                inputEle.onclick = function () {
+                    storeTimer(1500);
+                };
+                labelEle.for = "1500";
+                labelEle.innerText = "Every fifteen minutes";
+                divEle.append(inputEle);
+                divEle.append(labelEle);
+                break;
+            default:
+                console.log("Add Menu case error");
+                break;
         }
-        inputEle.onclick = function(){storeTimer(0);};
-        labelEle.for = "never";
-        labelEle.innerText = "Never";
-        divEle.append(inputEle);
-        divEle.append(labelEle);
-        divEle.append(brEle);
-        break;
-      case 1:
-        inputEle.type = "radio";
-        inputEle.id = "30";
-        inputEle.name = "timer";
-        inputEle.value = "30";
-        if (localStorage.getItem("refreshTimer") === 30) {
-          inputEle.checked = "checked";
-        }
-        inputEle.onclick = function(){storeTimer(30);};
-        labelEle.for = "30";
-        labelEle.innerText = "Every 30 seconds";
-        divEle.append(inputEle);
-        divEle.append(labelEle);
-        divEle.append(brEle);
-        break;
-      case 2:
-        inputEle.type = "radio";
-        inputEle.id = "120";
-        inputEle.name = "timer";
-        inputEle.value = "120";
-        if (localStorage.getItem("refreshTimer") === 120) {
-          inputEle.checked = "checked";
-        }
-        inputEle.onclick = function(){storeTimer(120);};
-        labelEle.for = "120";
-        labelEle.innerText = "Every two minutes";
-        divEle.append(inputEle);
-        divEle.append(labelEle);
-        divEle.append(brEle);
-        break;
-        case 3:
-          inputEle.type = "radio";
-          inputEle.id = "300";
-          inputEle.name = "timer";
-          inputEle.value = "300";
-          if (localStorage.getItem("refreshTimer") === 300) {
-            inputEle.checked = "checked";
-          }
-          inputEle.onclick = function(){storeTimer(300);};
-          labelEle.for = "300";
-          labelEle.innerText = "Every five minutes";
-          divEle.append(inputEle);
-          divEle.append(labelEle);
-          divEle.append(brEle);
-          break;
-        case 4:
-          inputEle.type = "radio";
-          inputEle.id = "1500";
-          inputEle.name = "timer";
-          inputEle.value = "1500";
-          if (localStorage.getItem("refreshTimer") === 1500) {
-            inputEle.checked = "checked";
-          }
-          inputEle.onclick = function(){storeTimer(1500);};
-          labelEle.for = "1500";
-          labelEle.innerText = "Every fifteen minutes";
-          divEle.append(inputEle);
-          divEle.append(labelEle);
-          break;
-      default:
-        console.log("Add Menu case error");
-        break;
     }
-  }
-  
-  document.body.append(divEle);
-} 
+
+    document.body.append(divEle);
+}
 
 /**
  * 
  * @returns void
  */
 function toggleMenu() {
-  let menuReveal = document.getElementById("service-console-extension-menu");
-  menuReveal.classList.toggle("service-console-extension-disabled");
+    let menuReveal = document.getElementById("service-console-extension-menu");
+    menuReveal.classList.toggle("service-console-extension-disabled");
 }
 
 /**
@@ -130,13 +158,13 @@ function toggleMenu() {
  * @returns void
  */
 function storeTimer(selection) {
-  if (typeof(Storage) !== "undefined") {
-    localStorage.setItem("refreshTimer", selection);
-    console.log(localStorage.getItem("refreshTimer"));
-  } else {
-    document.cookie = "refreshTimer=" + selection;
-    console.log(getCookie("refreshTimer"));
-  }
+    if (typeof (Storage) !== "undefined") {
+        localStorage.setItem("refreshTimer", selection);
+        console.log(localStorage.getItem("refreshTimer"));
+    } else {
+        document.cookie = "refreshTimer=" + selection;
+        console.log(getCookie("refreshTimer"));
+    }
 }
 
 /**
@@ -144,75 +172,51 @@ function storeTimer(selection) {
  * @returns void
  */
 function configuration() {
-  var widgetbtn = document.getElementById('service-console-extension-btn');
-  if (window.addEventListener) {
-    widgetbtn.addEventListener('click', toggleMenu, false);
-  } else if (window.attachEvent) {
-    widgetbtn.attachEvent('onclick', toggleMenu);
-  }
+    var widgetbtn = document.getElementById('service-console-extension-btn');
+    if (window.addEventListener) {
+        widgetbtn.addEventListener('click', toggleMenu, false);
+    } else if (window.attachEvent) {
+        widgetbtn.attachEvent('onclick', toggleMenu);
+    }
 }
 
 /**
  * 
- * @returns mixed
- */
-function mainbody() {
-  secondCounter = secondCounter + 1;
-  let utilityBar = document.querySelector(".oneUtilityBar.slds-utility-bar_container > ul");
-  if (utilityBar === null) {
-    return;
-  }
-  
-  let qTimer = 0;
-  switch(localStorage.getItem("refreshTimer")) {
-    case "0":
-      qTimer = 0;
-      break;
-    case "30":
-      qTimer = 30;
-      break;
-    case "120":
-      qTimer = 120;
-      break;
-    case "300":
-      qTimer = 300;
-      break;
-    case "1500":
-      qTimer = 1500;
-      break;
-    default:
-      console.log("qTimer == NaN");
-      break;
-  }
-
-  if (utilityBar.lastElementChild.innerText !== "SCE settings") {
-    let li = document.createElement("li");
-    li.id = "service-console-extension-btn";
-    li.innerText = "SCE settings";
-    utilityBar.append(li);
-    configuration();
-    addMenu();
-  }
-  
-  timerConsoleView(qTimer, secondCounter);
-}
-
-/**
- * 
- * @param {type} qTimer
  * @param {type} secondCounter 
  * @returns void
  */
-function timerConsoleView(qTimer, secondCounter){
-  console.log("Webpage last loaded: " + secondCounter);
-  console.log("Timer value: " + qTimer);
-  console.log("Refresh time: " + secondCounter % qTimer);
-  if (qTimer !== 0) {
-    if (secondCounter % qTimer === 0) {
-      sidebarRefresh();
-      tableRefresh();
+function queueTimer(secondCounter) {
+    let qTimer = 0;
+    switch (localStorage.getItem("refreshTimer")) {
+        case "0":
+            qTimer = 0;
+            break;
+        case "30":
+            qTimer = 30;
+            break;
+        case "120":
+            qTimer = 120;
+            break;
+        case "300":
+            qTimer = 300;
+            break;
+        case "1500":
+            qTimer = 1500;
+            break;
+        default:
+            console.log("qTimer == NaN");
+            break;
     }
-  } 
+    
+    console.log("Webpage last loaded: " + secondCounter);
+    console.log("Timer value: " + qTimer);
+    console.log("Refresh time: " + secondCounter % qTimer);
+    if (qTimer !== 0) {
+        if (secondCounter % qTimer === 0) {
+            sidebarRefresh();
+            tableRefresh();
+        }
+    }
 }
 
 
@@ -221,11 +225,11 @@ function timerConsoleView(qTimer, secondCounter){
  * @returns void
  */
 function sidebarRefresh() {
-  try {
-    document.querySelector(".slds-button-group > lightning-button-icon > button").click();
-  } catch(err) {
-    console.log(err);
-  }
+    try {
+        document.querySelector(".slds-button-group > lightning-button-icon > button").click();
+    } catch (err) {
+        console.log(err);
+    }
 }
 
 /**
@@ -233,22 +237,22 @@ function sidebarRefresh() {
  * @returns void
  */
 function tableRefresh() {
-  try {
-    document.querySelector(".sld-m-left_xx-small > div > lightning-button-icon > button > lightning-button-icon").click();
-  } catch(err) {
-    console.log(err);
-  }
-} 
+    try {
+        document.querySelector(".sld-m-left_xx-small > div > lightning-button-icon > button > lightning-button-icon").click();
+    } catch (err) {
+        console.log(err);
+    }
+}
 
 /**
  * 
  * @returns {undefined}
  */
 function feedRefresh() {
-  console.log("==> Enter feed refresh");
-  try {
-    document.querySelector(".feedActions > .right-actions > .slds-button").click();
-  } catch(err) {
-    console.log(err);
-  }
-} 
+    console.log("==> Enter feed refresh");
+    try {
+        document.querySelector(".feedActions > .right-actions > .slds-button").click();
+    } catch (err) {
+        console.log(err);
+    }
+}
---
+++ sce-settings-view/view.js
@@ -0,0 +1,12 @@
+"strict";
+
+/**
+ * Initials main body view so that it's added
+ * to the UI bottom right of the page.
+ * 
+ * @returns void
+ */
+setInterval(function () {
+    mainbody();
+}, 1000);
+
